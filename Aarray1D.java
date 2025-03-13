8debd05bf1ac74e03c9c8c22ce332382d36cb91b
ananndhitaa <anan.dita0724@gmail.com>
2025-03-13 08:05:02 +0700

    Update Aarray1D.java

--- Aarray1D.java
+++ Aarray1D.java
@@ -1,10 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pratikumTugas;
-
 /**
  Program : Array 1 Dimensi
  Tanggal : 07 maret 2025
